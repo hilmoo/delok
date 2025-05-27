54f41e68a8ceb46bb95a380b8f8a93c8bacf635c
hilmoo <hilmusyaf2005@gmail.com>
2025-05-27 02:31:54 +0000

    migrate to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart(),
-  ],
+  plugins: [react()],
 })
