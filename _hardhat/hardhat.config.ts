795f5abd7d8fbd24c1d5fbb9635cd931552c5472
hilmoo <hilmusyaf2005@gmail.com>
2025-06-03 12:54:41 +0000

    fix rpc issue

--- _hardhat/hardhat.config.ts
+++ _hardhat/hardhat.config.ts
@@ -6,8 +6,9 @@ import type { HardhatUserConfig } from "hardhat/config";
 const config: HardhatUserConfig = {
   solidity: "0.8.22",
   networks: {
-    hardhat: {
+    hardhathost: {
       chainId: 1337,
+      url: "http://host.docker.internal:8545",
     },
   },
 };
