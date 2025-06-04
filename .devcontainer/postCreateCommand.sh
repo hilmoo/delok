d5e2f3e27a56a0abcfce0062ea4dab2905e842d9
hilmoo <hilmusyaf2005@gmail.com>
2025-06-04 14:35:43 +0700

    update oracle

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,8 @@ npm install
 cd _hardhat
 rm -r ./ignition/deployments/
 echo "$NODE_RPC_URL" | npx hardhat vars set NODE_RPC_URL
+echo a | npx hardhat vars set INFURA_API_KEY
+echo a | npx hardhat vars set SEPOLIA_PRIVATE_KEY
 npm install
 npm run compile
 npm run deploy:lmselemes
